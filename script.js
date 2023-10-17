ac05167a340df6a85cebe60629900ec6a754b1a8
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-10-18 03:27:57 +0530

    added logic

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const inputs = document.querySelectorAll(".angle-input")
 const isTriangleBtn = document.querySelector("#is-triangle-btn")
 const outputElm = document.querySelector("#output")
 
+function calculateSumOfAngles(angle1,angle2,angle3){
+    const sumOfAngles = angle1+angle2+angle3
+    //console.log(sumOfAngles)
+    return sumOfAngles
+}
+
 function isTriangle(){
-   console.log("forms a triangle") 
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value))
+    if(sumOfAngles === 180){
+        outputElm.innerText = "Yay, The angles form a triangle"
+    }
+    else{
+        outputElm.innerText = "Oh Oh! The angles don't form a triangle"
+    }
 }
 
 isTriangleBtn.addEventListener("click", isTriangle)
