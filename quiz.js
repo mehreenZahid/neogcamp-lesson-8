9d80f02c7dbffd521acf80d6655c87c3310c14dd
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-10-31 00:25:07 +0530

    quiz.js added

--- quiz.js
+++ quiz.js
@@ -1,3 +1,13 @@
 const quizForm = document.querySelector(".quiz-form")
 const submitAnsBtn = document.querySelector("#submit-ans-btn")
 const outputElm = document.querySelector("output-quiz")
+
+const correctAnswers = ["90°","Right angled"]
+
+function calculateScore(){
+
+}
+
+submitAnsBtn.addEventListener("click",calculateScore){
+
+}
