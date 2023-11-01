902f22a048076c8ca34d505863e1dba71f7e6226
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-11-01 14:31:44 +0530

    added output text

--- quiz.js
+++ quiz.js
@@ -5,9 +5,17 @@ const outputElm = document.querySelector("output-quiz")
 const correctAnswers = ["90°","Right angled"]
 
 function calculateScore(){
-
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === correctAnswers[index]){
+            score = score+1
+        }
+        index = index+1
+    }
+   // console.log(score)
+    outputElm.innerText = "Your score is " + score
 }
 
-submitAnsBtn.addEventListener("click",calculateScore){
-
-}
+submitAnsBtn.addEventListener("click",calculateScore)
