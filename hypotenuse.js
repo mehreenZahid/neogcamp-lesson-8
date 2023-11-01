9bc1d43ffcf1355effbc2cd78063481d4467935c
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-11-01 23:27:25 +0530

    added hypotenuse.js

---
+++ hypotenuse.js
@@ -0,0 +1,9 @@
+const sides = document.querySelectorAll(".side-input")
+const hypotenuseBtn = document.querySelector("#hypotenuse-btn")
+const outputElm = document.querySelector("#output")
+
+function calculateHypotenuse(){
+    console.log("hypotenuse")
+}
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse)
