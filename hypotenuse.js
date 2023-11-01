9ae671c0b0efa38f5ec29a038def556f7c099706
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-11-02 01:14:45 +0530

    hypotenuse done

--- hypotenuse.js
+++ hypotenuse.js
@@ -2,8 +2,16 @@ const sides = document.querySelectorAll(".side-input")
 const hypotenuseBtn = document.querySelector("#hypotenuse-btn")
 const outputElm = document.querySelector("#output")
 
+function calculateSumOfSquares(a, b){
+    const sumOfSquares = a*a + b*b
+    //console.log(sumOfSquares)
+    return sumOfSquares
+}
+
 function calculateHypotenuse(){
-    console.log("hypotenuse")
+    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value))
+    const lengthOfHypotenuse = Math.sqrt(sumOfSquares)
+    outputElm.innerText = "The length of Hypotenuse is " + lengthOfHypotenuse
 }
 
 hypotenuseBtn.addEventListener("click", calculateHypotenuse)
